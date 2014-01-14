2f52db7df5e7befc650fcf000ea1eabd0653e340
lilipouille <lise.missillier@gmail.com>
2014-01-14 14:00:57 +0100

    page accueil ok
    css modifier et index.php
    jquery map et menu okay

--- index.php
+++ index.php
@@ -1,58 +1,60 @@
 <!doctype html>
-<html lang="en">
-	<head>
+<head>
 	<meta charset="utf-8">
-	<title>ICI</title>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+	<title>animate demo</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	<link rel="stylesheet" href="css/style.css">
-	<script>
-		$(function() {
-					 $( "#draggable5" ).draggable({ 
-						axis: "x", 
-						containment: "#containment-wrapper", 
-						scroll: false,
-						grid: [ 250,250 ] 
-					});
-		});
-	</script>
+	<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 	<script src="js/util.js"></script>
 </head>
 
 <html style="height: 100%;">
   <body style="height: 100%;">  
-	<div id="containment-wrapper" style="height: 100%; z-index: 2;">
-		<div class="draggable ui-widget-content" style="height: 95%; " id="draggable5" >
-			<header>
-                    <nav>
-                      <ul>
-                        <li id="compte"><a href="admin/">Mon compte</a></li>
-                        <li id="accueil"><a href="#">Accueil</a></li>
-                        <li id="revues"><a href="#">Revues</a></li>
-                        <li id="aPropos"><a href="#">A propos</a></li>
-                        <li id="aide"><a href="#">Aide</a></li>
-                      </ul>   
-                    </nav>
-                    <div id="logo"><img src="images/logo.png"/></div>
-                  </header>
-		</div>
-	</div>
-	
 	<?php require('id_connexion.php');	?>
+	<div class="menu" style="height: 90%; background-color: white;">
+		<div class="navarrow" style="">
+			<button id="ouv">Ouvert</button> 
+			<button id="miouv">Mi Ouvert</button>	
+			<button id="ferm">Ferme</button>	
+		</div>
+		<header>
+			<nav>
+			  <ul>
+				<li id="compte"><a href="admin/">Mon compte</a></li>
+				<li id="accueil"><a href="#">Accueil</a></li>
+				<li id="revues"><a href="#">Revues</a></li>
+				<li id="aPropos"><a href="#">A propos</a></li>
+				<li id="aide"><a href="#">Aide</a></li>
+			  </ul>   
+			</nav>
+			<div id="logo"><img src="images/logo.png"/></div>
+		</header>
+	</div>
 
-		<section id="content" style="height: 100%; ">
-			<div style="height: 100%; ">
-				<div id="map-canvas" style="height: 95%; "></div>
-
-				<?php
-					$sql='SELECT * FROM map;';
-					$req=$bdd->query($sql);
-					require('map.php');
-				?>
-			</div>
-		</section>
-</body>
+	<section id="content" style="height: 100%;">
+		<div id="map-canvas" style="height: 90%;"></div>
+			<?php
+				$sql='SELECT * FROM map;';
+				$req=$bdd->query($sql);
+				require('map.php');
+			?>
+		</div>
+	</section>
 	
+	
+	<script>		
+		$( "#ouv" ).click(function() {
+			$( ".menu" ).animate({ "left": "0" }, "slow" );
+		});
+		
+		$( "#miouv" ).click(function() {
+			$( ".menu" ).animate({ "left": "-20%" }, "slow" );
+		});
+		
+		$( "#ferm" ).click(function(){
+			$( ".menu" ).animate({ "left": "-35%" }, "slow" );
+		});
+	</script>
+  </body>
 </html>
