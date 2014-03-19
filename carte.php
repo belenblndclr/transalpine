5b704249a7a0e9494aaab370676ae841610dec92
lilipouille <lise.missillier@gmail.com>
2014-03-19 23:34:10 +0100

    CSS LastChild
    23h30 !

--- carte.php
+++ carte.php
@@ -37,33 +37,33 @@
 		<div class="form">
 			<form>
 				<fieldset>
-				<legend>Régions</legend>
-					<input type="checkbox" name="region" id="savoie" value="1" /> <label for="savoie">Savoie</label>
-					<input type="checkbox" name="region" id="piemont" value="2"/> <label for="piemont">Piémont</label>
-					<input type="checkbox" name="region" id="dauphine" value="3"/> <label for="dauphine">Dauphiné</label>
-					<input type="checkbox" name="region" id="suisse" value="4"/> <label for="suisse">Suisse</label>
+					<legend>Régions</legend>
+						<input type="checkbox" name="region" id="savoie" value="1" /> <label for="savoie">Savoie</label>
+						<input type="checkbox" name="region" id="piemont" value="2"/> <label for="piemont">Piémont</label>
+						<input type="checkbox" name="region" id="dauphine" value="3"/> <label for="dauphine">Dauphiné</label>
+						<input type="checkbox" name="region" id="suisse" value="4"/> <label for="suisse">Suisse</label>
 				</fieldset>
 				<fieldset>
-				<legend>Siècles</legend>
-					<input type="text" id="siecle" name="rangeName" value="10;100"/>
+					<legend>Siècles</legend>
+						<input type="text" id="siecle" name="rangeName" value="10;100"/>
 				</fieldset>
 				<fieldset>
-				<legend>Auteur</legend>
-					<input type="checkbox" name="ad" id="ad" /> <label for="ad">a-d</label>
-					<input type="checkbox" name="eh" id="eh" /> <label for="eh">e-h</label>
-					<input type="checkbox" name="il" id="il" /> <label for="il">i-l</label>
-					<input type="checkbox" name="mp" id="mp" /> <label for="mp">m-p</label>
-					<input type="checkbox" name="qt" id="qt" /> <label for="qt">q-t</label>
-					<input type="checkbox" name="uz" id="uz" /> <label for="uz">u-z</label>
+					<legend>Auteur</legend>
+						<input type="checkbox" name="ad" id="ad" /> <label for="ad">a-d</label>
+						<input type="checkbox" name="eh" id="eh" /> <label for="eh">e-h</label>
+						<input type="checkbox" name="il" id="il" /> <label for="il">i-l</label>
+						<input type="checkbox" name="mp" id="mp" /> <label for="mp">m-p</label>
+						<input type="checkbox" name="qt" id="qt" /> <label for="qt">q-t</label>
+						<input type="checkbox" name="uz" id="uz" /> <label for="uz">u-z</label>
 				</fieldset>
 				<fieldset>
-				<legend>Typologie</legend>
-					<input type="checkbox" name="compta" id="compta" /> <label for="compta">Comptabilité</label>
-					<input type="checkbox" name="chartre" id="chartre" /> <label for="chartre">Chartres</label>
+					<legend>Typologie</legend>
+						<input type="checkbox" name="compta" id="compta" /> <label for="compta">Comptabilité</label>
+						<input type="checkbox" name="chartre" id="chartre" /> <label for="chartre">Chartres</label>
 				</fieldset>
 				<fieldset>
-				<legend>Référence</legend>
-					<input type="text" name="ref" id="ref" /> <label for="ref">Choisissez</label>
+					<legend>Référence</legend>
+						<input type="text" name="ref" id="ref" /> <label for="ref">Choisissez</label>
 				</fieldset>
 			</form>
 		</div>
@@ -98,11 +98,11 @@
 		});
 		
 		$( "#miouv" ).click(function() {
-			$( ".menu" ).animate({ "left": "-20%" }, "slow" );
+			$( ".menu" ).animate({ "left": "-15em" }, "slow" );
 		});
 		
 		$( "#ferm" ).click(function(){
-			$( ".menu" ).animate({ "left": "-30%" }, "slow" );
+			$( ".menu" ).animate({ "left": "-25em" }, "slow" );
 		});
 	</script>
 	
